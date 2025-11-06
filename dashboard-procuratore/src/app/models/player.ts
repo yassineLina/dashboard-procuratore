11f39916d55eeaf316dc5f9be47d04a3208e37fe
yassineelkharmoudi <yassine.elkharmoudi.cons@autorita-trasporti.it>
2025-11-06 10:31:42 +0100

    aggiunto modello player

---
+++ dashboard-procuratore/src/app/models/player.ts
@@ -0,0 +1,14 @@
+export interface Player {
+  id: number;
+  name: string;
+  surname: string;
+  age: number;
+  nationality: string;
+  position: string;
+  currentTeam: string;
+  marketValue: number;
+  contractExpiry: Date;
+  phoneNumber: string;
+  email: string;
+  agentNotes: string;
+}
