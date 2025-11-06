0af214329db9beb09c11375003f8bab910bbaaf8
yassineelkharmoudi <yassine.elkharmoudi.cons@autorita-trasporti.it>
2025-11-06 17:36:38 +0100

    Fix prerendering error and add workflow

--- dashboard-procuratore/src/app/app.routes.server.ts
+++ dashboard-procuratore/src/app/app.routes.server.ts
@@ -4,5 +4,5 @@ export const serverRoutes: ServerRoute[] = [
   { path: '', renderMode: RenderMode.Prerender },
   { path: 'dashboard', renderMode: RenderMode.Prerender },
   { path: 'players', renderMode: RenderMode.Prerender },
-  { path: '**', renderMode: RenderMode.Server }
+  { path: '**', renderMode: RenderMode.Prerender  }
 ];
