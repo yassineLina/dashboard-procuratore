d34425db2d673ad5d9127d961bf3be886d002ee0
yassineelkharmoudi <yassine.elkharmoudi.cons@autorita-trasporti.it>
2025-11-06 15:14:15 +0100

    ci

--- dashboard-procuratore/src/app/app.routes.server.ts
+++ dashboard-procuratore/src/app/app.routes.server.ts
@@ -1,8 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
-  {
-    path: '**',
-    renderMode: RenderMode.Prerender
-  }
+  { path: '', renderMode: RenderMode.Prerender },
+  { path: 'dashboard', renderMode: RenderMode.Prerender },
+  { path: 'players', renderMode: RenderMode.Prerender },
+  { path: '**', renderMode: RenderMode.Server }
 ];
